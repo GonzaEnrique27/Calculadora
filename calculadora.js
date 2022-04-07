a025f1bb5df97da3028374524c920d8ad37cd49f
GonzaEnrique27 <Gonz_1994@hotmail.com>
2022-04-07 20:26:48 -0300

    calculadora

---
+++ calculadora.js
@@ -0,0 +1,8 @@
+let restar = require ('./Calculadora/restar')
+
+let sumar = require ('./Calculadora/suma')
+
+let multiplicacion = require ('./Calculadora/multiplicacion')
+
+let division = require ('./Calculadora/multiplicacion')
+
